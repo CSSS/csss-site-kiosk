acfdb875fb12cce8fc894b46bb17759d34d7cd57
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-23 19:12:41 -0700

    chore: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  csssApiUrl: 'http://localhost:3049/api',
+  appUrl: 'http://localhost:4200'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  csssApiUrl: 'https://sfucsss.org/api',
+  appUrl: 'https://kiosk.sfucsss.org'
+};
