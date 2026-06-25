5333a052d8ccfacaa8184c8d1f85b98784823433
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-24 23:22:52 -0700

    feat(event): swiping through widgets works

--- src/app/widgets/event/event.widget.ts
+++ src/app/widgets/event/event.widget.ts
@@ -1,5 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  viewChild
+} from '@angular/core';
 
 interface Event {
   posterUrl: string;
@@ -10,10 +16,13 @@ interface Event {
 @Component({
   selector: 'ksk-event-widget',
   imports: [NgOptimizedImage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './event.widget.html',
   styleUrl: './event.widget.scss'
 })
-export class EventWidget {
+export class EventWidget implements AfterViewInit {
+  swiperRef = viewChild.required<ElementRef>('swiperRef');
+
   events: Event[] = [
     {
       posterUrl: 'https://dummyimage.com/380x500/000/fff',
@@ -29,6 +38,43 @@ export class EventWidget {
       posterUrl: 'https://dummyimage.com/380x500/000/fff',
       title: 'Third Poster',
       location: 'Location'
+    },
+    {
+      posterUrl: 'https://dummyimage.com/380x500/000/fff',
+      title: 'First Poster',
+      location: 'Location'
+    },
+    {
+      posterUrl: 'https://dummyimage.com/380x500/000/fff',
+      title: 'Second Poster',
+      location: 'Location'
+    },
+    {
+      posterUrl: 'https://dummyimage.com/380x500/000/fff',
+      title: 'Third Poster',
+      location: 'Location'
+    },
+    {
+      posterUrl: 'https://dummyimage.com/380x500/000/fff',
+      title: 'Third Poster',
+      location: 'Location'
     }
   ];
+
+  ngAfterViewInit(): void {
+    const swiperEl = this.swiperRef().nativeElement;
+
+    const swiperParams = {
+      slidesPerView: 3,
+      centeredSlides: true,
+      loop: true,
+      spaceBetween: 10,
+      grabCursor: true,
+      touchRatio: 1,
+      resistanceRatio: 0.5
+    };
+
+    Object.assign(swiperEl, swiperParams);
+    swiperEl.initialize();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { register } from 'swiper/element';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+register();
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
