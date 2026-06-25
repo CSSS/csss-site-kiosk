8093fca46f843316856383edeb682867e01285fd
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-25 09:33:42 -0700

    feat(bus): add timers and status to widget

--- src/app/widgets/bus-schedule/bus-schedule.widget.ts
+++ src/app/widgets/bus-schedule/bus-schedule.widget.ts
@@ -1,20 +1,30 @@
-import { DecimalPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { catchError, map, of, switchMap, timer } from 'rxjs';
+import { catchError, combineLatest, map, of, shareReplay, switchMap, timer } from 'rxjs';
+import { BusStatus, type TransLinkScheduleResponse } from '../../api/generated/csss-backend';
 import { TranslinkService } from '../../api/translink/translink.service';
 import { TimeService } from '../../core/time.service';
 
-const TWO_MINUTES = 1000 * 60 * 2;
+const ONE_MINUTE = 1000 * 60;
+const TWO_MINUTES = ONE_MINUTE * 2;
 
 interface DepartureInfo {
   routeNumber: string;
   secondsUntilDeparture: number;
+  status: string;
 }
+
+// TODO: Fix the enum values on the backend and regenerate the services to get better enum names
+const STATUS_COLOUR_MAP: Record<number, string> = {
+  [BusStatus.NUMBER_1]: 'status--arrived',
+  [BusStatus.NUMBER_2]: 'status--delayed',
+  [BusStatus.NUMBER_3]: 'status--on-time',
+  [BusStatus.NUMBER_4]: 'status--cancelled'
+};
 
 @Component({
   selector: 'ksk-bus-schedule-widget',
-  imports: [DecimalPipe],
+  imports: [],
   templateUrl: './bus-schedule.widget.html',
   styleUrl: './bus-schedule.widget.scss'
 })
@@ -22,31 +32,38 @@ export class BusScheduleWidget {
   private translinkService = inject(TranslinkService);
   private timeService = inject(TimeService);
 
-  nextDepartures = toSignal(
-    timer(0, TWO_MINUTES).pipe(
-      switchMap(() =>
-        this.translinkService.getNextDepartures().pipe(
-          map(res => {
-            const now = this.timeService.currentTime();
-            const result: DepartureInfo[] = [];
-            for (const departure of res.values()) {
-              result.push({
-                routeNumber: departure.route_number,
-                secondsUntilDeparture: Math.floor(
-                  departure.scheduled_departure_time - now.getTime() / 1000
-                )
-              });
-            }
-            return result;
-          }),
-          catchError(error => {
-            console.error('Error while polling departures:', error);
-            return of([] as DepartureInfo[]);
-          })
-        )
+  private pollDepartures$ = timer(0, TWO_MINUTES).pipe(
+    switchMap(() =>
+      this.translinkService.getNextDepartures().pipe(
+        catchError(error => {
+          console.error('Error while polling departures:', error);
+          return of([] as TransLinkScheduleResponse[]);
+        })
       )
     ),
-    { initialValue: [] }
+    shareReplay({ bufferSize: 1, refCount: true, windowTime: TWO_MINUTES })
+  );
+
+  protected nextDepartures = toSignal(
+    combineLatest([this.pollDepartures$, timer(0, ONE_MINUTE)]).pipe(
+      map(([res]) => {
+        const now = this.timeService.currentTime();
+        const result: DepartureInfo[] = [];
+        for (const departure of res.values()) {
+          result.push({
+            routeNumber: departure.route_number,
+            secondsUntilDeparture: Math.floor(
+              departure.scheduled_departure_time - now.getTime() / 1000
+            ),
+            status: STATUS_COLOUR_MAP[departure.status]
+          });
+        }
+        return result;
+      })
+    ),
+    {
+      initialValue: []
+    }
   );
 
   protected getDisplayTime(timeDiff: number): string {
