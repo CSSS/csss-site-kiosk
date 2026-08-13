7dcb0c1db721fec84818c1c4ec7cfe24bb8aac70
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-13 13:21:41 -0700

    fix: remove @napi-rs/keyring and use environment variables

--- server/secrets/index.js
+++
@@ -1,6 +0,0 @@
-const provider =
-  process.env.NODE_ENV === 'production'
-    ? await import('./secrets.production.js')
-    : await import('./secrets.development.js');
-
-export const { getSecret } = provider;
--- server/secrets/secrets.development.js
+++
@@ -1,7 +0,0 @@
-export function getSecret() {
-  const secret = process.env.KIOSK_API_SECRET;
-  if (!secret) {
-    throw new Error('`KIOSK_API_SECRET` not set in .env.');
-  }
-  return secret;
-}
