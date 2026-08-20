ba58d0ff8299f149ecc67c01a3bc2745a2b5f2da
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-20 02:59:19 -0700

    feat: use production database openapi.json to generate builds

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  csssApiUrl: 'http://localhost:3049/api',
+  csssApiUrl: 'http://localhost:3049',
   appUrl: 'http://localhost:8080',
   mediaUrl: '/images'
 };
