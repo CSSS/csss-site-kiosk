4b40e7b8f5d9c2c9a2e55c3923defdff8921e626
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-20 11:58:04 -0700

    fix: fix prod environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  csssApiUrl: 'http://localhost:8080/api',
+  csssApiUrl: 'http://localhost:8080',
   appUrl: 'http://localhost:8080',
   mediaUrl: 'https://sfucsss.org/media'
 };
