d76e91c12801b9950108d2b499871ea65e7ae5d1
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-09-20 15:26:11 -0700

    fix(event modal): layout now based off a fixed image size

--- frontend/src/app/screens/events/events-modal/events-modal.component.ts
+++ frontend/src/app/screens/events/events-modal/events-modal.component.ts
@@ -8,8 +8,17 @@ import { KioskEvent } from '../event.types';
   imports: [NgOptimizedImage, QRCodeComponent, EventDetailsComponent],
   selector: 'ksk-events-modal',
   styleUrl: './events-modal.component.scss',
-  templateUrl: './events-modal.component.html'
+  templateUrl: './events-modal.component.html',
+  host: {
+    '[style.--img-width]': 'imgWidth + "px"',
+    '[style.--img-height]': 'imgHeight + "px"',
+    '[style.--qr-code-size]': 'qrCodeSize + "px"'
+  }
 })
 export class EventsModalComponent {
   protected readonly event = input.required<KioskEvent>();
+
+  imgHeight = 375;
+  imgWidth = this.imgHeight * (4 / 5);
+  qrCodeSize = 160;
 }
